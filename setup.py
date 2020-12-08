b311e19a39b0abf734fb92d1596846649ff2b271
hagen <michael.hagen@infoteam.de>
2020-12-08 14:30:38 +0100

    fix: Change Project Name in setup.py
    Change project name in setup.py from 'src' to 'it_ticket_analysis' in order to have it listed in pip freeze under that name.

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import find_packages, setup
 
+# Todo: Add license and description texts
 setup(
-    name='src',
+    name='it_ticket_analysis',
     packages=find_packages(),
     version='0.1.0',
     description='TODO',
