cc3f8a9081595f4fed0f04b359514aff189739d2
hagen <michael.hagen@infoteam.de>
2021-01-12 12:25:23 +0100

    refactor: change depth of rf to 2

--- src/it_ticket_analysis/train.py
+++ src/it_ticket_analysis/train.py
@@ -27,7 +27,7 @@ X_train, X_test, y_train, y_test = train_test_split(df, y, test_size=0.2, random
 #################################
 
 # Fit a model on the train section
-clf = RandomForestClassifier(max_depth=6, random_state=seed)
+clf = RandomForestClassifier(max_depth=2, random_state=seed)
 clf.fit(X_train, y_train)
 
 # Report training set score
@@ -36,8 +36,8 @@ train_score = clf.score(X_train, y_train) * 100
 test_score = clf.score(X_test, y_test) * 100
 
 with open("metrics.json", 'w') as outfile:
-        json.dump({ "Training Accuracy": train_score,
-                    "Test Accuracy": test_score}, outfile)
+    json.dump({"Training Accuracy": train_score,
+               "Test Accuracy": test_score}, outfile)
 
 
 # Calc ROC
