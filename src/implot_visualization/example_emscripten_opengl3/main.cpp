5624da120175c603355bb455c5c4546e99efa169
Restyled.io <commits@restyled.io>
2022-08-03 08:22:44 +0000

    Restyled by clang-format

--- src/implot_visualization/example_emscripten_opengl3/main.cpp
+++ src/implot_visualization/example_emscripten_opengl3/main.cpp
@@ -7,26 +7,26 @@
 // It is possible to combine both code into a single source file that will compile properly on Desktop and using Emscripten.
 // See https://github.com/ocornut/imgui/pull/2492 as an example on how to do just that.
 
-#include <iostream>
 #include "imgui.h"
-#include "imgui_impl_sdl.h"
 #include "imgui_impl_opengl3.h"
-#include <stdio.h>
-#include <string.h>
+#include "imgui_impl_sdl.h"
 #include <emscripten.h>
+#include <emscripten/fetch.h>
+#include <implot.h>
+#include <iostream>
+#include <nlohmann/json.hpp>
 #include <SDL.h>
 #include <SDL_opengles2.h>
-#include <implot.h>
-#include <emscripten/fetch.h>
-#include <nlohmann/json.hpp>
+#include <stdio.h>
+#include <string.h>
 // #include <IoSerialiserJson.hpp>
 
 using json = nlohmann::json;
 
 // Utility structure for realtime plot
 struct ScrollingBuffer {
-    int MaxSize;
-    int Offset;
+    int              MaxSize;
+    int              Offset;
     ImVector<ImVec2> Data;
     ScrollingBuffer(int max_size = 2000) {
         MaxSize = max_size;
@@ -35,30 +35,30 @@ struct ScrollingBuffer {
     }
     void AddPoint(float x, float y) {
         if (Data.size() < MaxSize)
-            Data.push_back(ImVec2(x,y));
+            Data.push_back(ImVec2(x, y));
         else {
-            Data[Offset] = ImVec2(x,y);
-            Offset =  (Offset + 1) % MaxSize;
+            Data[Offset] = ImVec2(x, y);
+            Offset       = (Offset + 1) % MaxSize;
         }
     }
     void Erase() {
         if (Data.size() > 0) {
             Data.shrink(0);
-            Offset  = 0;
+            Offset = 0;
         }
     }
-}; 
+};
 
 // Implot Test data
-int bar_data[5] = {1, 2, 3, 4, 5};
-int x_data[5] = {1, 2, 3, 4, 5};
-int y1_data[5] = {4, 6, 8, 5, 3};
-int y2_data[5] = {3, 4, 8, 1, 2};
+int             bar_data[5] = { 1, 2, 3, 4, 5 };
+int             x_data[5]   = { 1, 2, 3, 4, 5 };
+int             y1_data[5]  = { 4, 6, 8, 5, 3 };
+int             y2_data[5]  = { 3, 4, 8, 1, 2 };
 ScrollingBuffer buffer;
 
 // Deserialise
 // dataAsJson:  String of format {"key1": value1, "key2": value2, ...}
-void deserialiseJson(std::string jsonString){
+void deserialiseJson(std::string jsonString) {
     int tmp_x;
     int tmp_y;
 
@@ -71,15 +71,13 @@ void deserialiseJson(std::string jsonString){
     std::cout << "Modified Json string: " << modifiedJsonString << "\n";
 
     auto json_obj = json::parse(modifiedJsonString);
-    for (auto& element : json_obj.items()) {
+    for (auto &element : json_obj.items()) {
         std::cout << "Json elements: key: " << element.key() << ", value: " << element.value() << "\n";
         if (element.key() == "count") {
             tmp_x = element.value();
-        }
-        else {
+        } else {
             tmp_y = element.value();
         }
-            
     }
     std::cout << "Buffer: x: " << tmp_x << ", y: " << tmp_y << "\n";
     buffer.AddPoint(tmp_x, tmp_y);
@@ -87,36 +85,34 @@ void deserialiseJson(std::string jsonString){
 }
 
 void downloadSucceeded(emscripten_fetch_t *fetch) {
-  printf("Finished downloading %llu bytes from URL %s.\n", fetch->numBytes, fetch->url);
-  // The data is now available at fetch->data[0] through fetch->data[fetch->numBytes-1];
-  std::string jsonData;
-  for (int i=0; i<fetch->numBytes; i++){
-    jsonData += fetch->data[i];
-  }
-  
-  printf("Json string:\n%s\n",jsonData.c_str());
-  deserialiseJson(jsonData.c_str());
-  emscripten_fetch_close(fetch); // Free data associated with the fetch.
+    printf("Finished downloading %llu bytes from URL %s.\n", fetch->numBytes, fetch->url);
+    // The data is now available at fetch->data[0] through fetch->data[fetch->numBytes-1];
+    std::string jsonData;
+    for (int i = 0; i < fetch->numBytes; i++) {
+        jsonData += fetch->data[i];
+    }
+
+    printf("Json string:\n%s\n", jsonData.c_str());
+    deserialiseJson(jsonData.c_str());
+    emscripten_fetch_close(fetch); // Free data associated with the fetch.
 }
 
 void downloadFailed(emscripten_fetch_t *fetch) {
-  printf("Downloading %s failed, HTTP failure status code: %d.\n", fetch->url, fetch->status);
-  emscripten_fetch_close(fetch); // Also free data on failure.
+    printf("Downloading %s failed, HTTP failure status code: %d.\n", fetch->url, fetch->status);
+    emscripten_fetch_close(fetch); // Also free data on failure.
 }
 
 // Emscripten requires to have full control over the main loop. We're going to store our SDL book-keeping variables globally.
 // Having a single function that acts as a loop prevents us to store state in the stack of said function. So we need some location for this.
-SDL_Window*     g_Window = NULL;
-SDL_GLContext   g_GLContext = NULL;
+SDL_Window   *g_Window    = NULL;
+SDL_GLContext g_GLContext = NULL;
 
 // For clarity, our main loop code is declared at the end.
-static void main_loop(void*);
+static void main_loop(void *);
 
-int main(int, char**)
-{
+int         main(int, char **) {
     // Setup SDL
-    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0)
-    {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_GAMECONTROLLER) != 0) {
         printf("Error: %s\n", SDL_GetError());
         return -1;
     }
@@ -124,8 +120,8 @@ int main(int, char**)
     // For the browser using Emscripten, we are going to use WebGL1 with GL ES2. See the Makefile. for requirement details.
     // It is very likely the generated file won't work in many browsers. Firefox is the only sure bet, but I have successfully
     // run this code on Chrome for Android for example.
-    const char* glsl_version = "#version 100";
-    //const char* glsl_version = "#version 300 es";
+    const char *glsl_version = "#version 100";
+    // const char* glsl_version = "#version 300 es";
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
@@ -137,11 +133,10 @@ int main(int, char**)
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
     SDL_DisplayMode current;
     SDL_GetCurrentDisplayMode(0, &current);
-    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    g_Window = SDL_CreateWindow("Dear ImGui Emscripten example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
-    g_GLContext = SDL_GL_CreateContext(g_Window);
-    if (!g_GLContext)
-    {
+    SDL_WindowFlags window_flags = (SDL_WindowFlags) (SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+    g_Window                     = SDL_CreateWindow("Dear ImGui Emscripten example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+    g_GLContext                  = SDL_GL_CreateContext(g_Window);
+    if (!g_GLContext) {
         fprintf(stderr, "Failed to initialize WebGL context!\n");
         return 1;
     }
@@ -151,9 +146,10 @@ int main(int, char**)
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImPlot::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    ImGuiIO &io = ImGui::GetIO();
+    (void) io;
+    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
 
     // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
     // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
@@ -161,7 +157,7 @@ int main(int, char**)
 
     // Setup Dear ImGui style
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
+    // ImGui::StyleColorsClassic();
 
     // Setup Platform/Renderer backends
     ImGui_ImplSDL2_InitForOpenGL(g_Window, g_GLContext);
@@ -175,37 +171,36 @@ int main(int, char**)
     // - Read 'docs/FONTS.md' for more instructions and details.
     // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
     // - Emscripten allows preloading a file or folder to be accessible at runtime. See Makefile for details.
-    //io.Fonts->AddFontDefault();
+    // io.Fonts->AddFontDefault();
 #ifndef IMGUI_DISABLE_FILE_FUNCTIONS
     io.Fonts->AddFontFromFileTTF("fonts/Roboto-Medium.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("fonts/Cousine-Regular.ttf", 15.0f);
-    //io.Fonts->AddFontFromFileTTF("fonts/DroidSans.ttf", 16.0f);
-    //io.Fonts->AddFontFromFileTTF("fonts/ProggyTiny.ttf", 10.0f);
-    //ImFont* font = io.Fonts->AddFontFromFileTTF("fonts/ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
-    //IM_ASSERT(font != NULL);
+    // io.Fonts->AddFontFromFileTTF("fonts/Cousine-Regular.ttf", 15.0f);
+    // io.Fonts->AddFontFromFileTTF("fonts/DroidSans.ttf", 16.0f);
+    // io.Fonts->AddFontFromFileTTF("fonts/ProggyTiny.ttf", 10.0f);
+    // ImFont* font = io.Fonts->AddFontFromFileTTF("fonts/ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    // IM_ASSERT(font != NULL);
 #endif
 
     // This function call won't return, and will engage in an infinite loop, processing events from the browser, and dispatching them.
     emscripten_set_main_loop_arg(main_loop, NULL, 0, true);
 }
 
-static void main_loop(void* arg)
-{
+static void main_loop(void *arg) {
     // Use emscripten Fetch API as long polling mechanism
     emscripten_fetch_attr_t attr;
     emscripten_fetch_attr_init(&attr);
     strcpy(attr.requestMethod, "GET");
     attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY;
-    attr.onsuccess = downloadSucceeded;
-    attr.onerror = downloadFailed;
+    attr.onsuccess  = downloadSucceeded;
+    attr.onerror    = downloadFailed;
     emscripten_fetch(&attr, "http://localhost:8080/testCounter");
 
-    ImGuiIO& io = ImGui::GetIO();
+    ImGuiIO &io = ImGui::GetIO();
     IM_UNUSED(arg); // We can pass this argument as the second parameter of emscripten_set_main_loop_arg(), but we don't use that.
 
     // Our state (make them static = more or less global) as a convenience to keep the example terse.
-    static bool show_demo_window = true;
-    static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    static bool   show_demo_window = true;
+    static ImVec4 clear_color      = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     // Poll and handle events (inputs, window resize, etc.)
     // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
@@ -213,8 +208,7 @@ static void main_loop(void* arg)
     // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
     // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
     SDL_Event event;
-    while (SDL_PollEvent(&event))
-    {
+    while (SDL_PollEvent(&event)) {
         ImGui_ImplSDL2_ProcessEvent(&event);
         // Capture events here, based on io.WantCaptureMouse and io.WantCaptureKeyboard
     }
@@ -225,22 +219,22 @@ static void main_loop(void* arg)
     ImGui::NewFrame();
 
     // Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-    if (show_demo_window){
+    if (show_demo_window) {
         ImGui::ShowDemoWindow(&show_demo_window);
         ImPlot::ShowDemoWindow();
     }
 
     // Show counter demo
     if (buffer.Data.size() > 0) {
-        ImGui::SetNextWindowSize(ImVec2(800,300), ImGuiCond_Appearing);
-        int bufferEnd = buffer.Data.size()-1;
+        ImGui::SetNextWindowSize(ImVec2(800, 300), ImGuiCond_Appearing);
+        int bufferEnd = buffer.Data.size() - 1;
         ImGui::Begin("Counter Demo Window");
-        if(ImPlot::BeginPlot("Counter Worker")){
-            ImPlot::SetupAxes("","Value");
-            ImPlot::SetupAxisLimits(ImAxis_X1,buffer.Data[bufferEnd].x-250.0, buffer.Data[bufferEnd].x, ImGuiCond_Always);
-            ImPlot::SetupAxisLimits(ImAxis_Y1,0,100);
-            ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL,0.5f);
-            ImPlot::PlotLine("Counter", &buffer.Data[0].x, &buffer.Data[0].y, buffer.Data.size(), 0, buffer.Offset, 2*sizeof(float));
+        if (ImPlot::BeginPlot("Counter Worker")) {
+            ImPlot::SetupAxes("", "Value");
+            ImPlot::SetupAxisLimits(ImAxis_X1, buffer.Data[bufferEnd].x - 250.0, buffer.Data[bufferEnd].x, ImGuiCond_Always);
+            ImPlot::SetupAxisLimits(ImAxis_Y1, 0, 100);
+            ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL, 0.5f);
+            ImPlot::PlotLine("Counter", &buffer.Data[0].x, &buffer.Data[0].y, buffer.Data.size(), 0, buffer.Offset, 2 * sizeof(float));
             ImPlot::EndPlot();
         }
         ImGui::End();
@@ -263,11 +257,10 @@ static void main_loop(void* arg)
     //     ImGui::End();
     // }
 
-
     // Rendering
     ImGui::Render();
     SDL_GL_MakeCurrent(g_Window, g_GLContext);
-    glViewport(0, 0, (int)io.DisplaySize.x, (int)io.DisplaySize.y);
+    glViewport(0, 0, (int) io.DisplaySize.x, (int) io.DisplaySize.y);
     glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
     glClear(GL_COLOR_BUFFER_BIT);
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
