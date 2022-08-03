d68d53da390c98f2307a391e05c5eff2fda54581
johannaheuser <johanna.heuser@infoteam.de>
2022-08-03 15:27:58 +0200

    Format code using clang-format-13

--- src/opencmw_counter/README.md
+++ src/opencmw_counter/README.md
@@ -4,11 +4,12 @@ This is a minimal example of a worker implementation using opencmw that simulate
 
 ### Building
 
-``` bash
+```bash
 cmake -S . -B build
 cmake --build build
 build/src/opencmw_counter
 ```
+
 ### Building against local opencmw-cpp
 
 Note: If you just want to implement a service, it will not be required and brings no benefit to checkout and build opencmw-cpp separatly.
@@ -18,7 +19,7 @@ There are environment variables which optionally can be used to point to your ch
 
 To do so, refer to the top folder of the other projects (which contains the toplevel `CMakeList.txt`) by doing:
 
-``` bash
+```bash
 cmake -S . -B build -DFETCHCONTENT_SOURCE_DIR_FMT=~/libfmt -DFETCHCONTENT_SOURCE_DIR_OPENCMW-CPP=~/opencmw-cpp
 ```
 
--- src/opencmw_counter/src/opencmw_counter.cpp
+++ src/opencmw_counter/src/opencmw_counter.cpp
@@ -1,7 +1,7 @@
+#include <majordomo/base64pp.hpp>
 #include <majordomo/Broker.hpp>
 #include <majordomo/RestBackend.hpp>
 #include <majordomo/Worker.hpp>
-#include <majordomo/base64pp.hpp>
 
 #include <atomic>
 #include <fstream>
@@ -13,31 +13,30 @@ using namespace opencmw::majordomo;
 CMRC_DECLARE(assets);
 
 // from restserver_testapp.cpp
-template <typename Mode, typename VirtualFS, role... Roles>
+template<typename Mode, typename VirtualFS, role... Roles>
 class FileServerRestBackend : public RestBackend<Mode, VirtualFS, Roles...> {
-  private:
+private:
     using super_t = RestBackend<Mode, VirtualFS, Roles...>;
     std::filesystem::path _serverRoot;
     using super_t::_svr;
     using super_t::DEFAULT_REST_SCHEME;
 
-  public:
+public:
     using super_t::RestBackend;
 
     FileServerRestBackend(
-        Broker<Roles...> &broker, const VirtualFS &vfs,
-        std::filesystem::path serverRoot,
-        opencmw::URI<> restAddress = opencmw::URI<>::factory()
-                                     .scheme(DEFAULT_REST_SCHEME)
-                                     .hostName("0.0.0.0")
-                                     .port(DEFAULT_REST_PORT)
-                                     .build())
-        : super_t(broker, vfs, restAddress), 
-          _serverRoot(std::move(serverRoot)) {}
+            Broker<Roles...> &broker, const VirtualFS &vfs,
+            std::filesystem::path serverRoot,
+            opencmw::URI<>        restAddress = opencmw::URI<>::factory()
+                                                 .scheme(DEFAULT_REST_SCHEME)
+                                                 .hostName("0.0.0.0")
+                                                 .port(DEFAULT_REST_PORT)
+                                                 .build())
+        : super_t(broker, vfs, restAddress), _serverRoot(std::move(serverRoot)) {}
 
     static auto
     deserializeSemicolonFormattedMessage(std::string_view method,
-                                         std::string_view serialized) {
+            std::string_view                              serialized) {
         // clang-format off
         auto result = MdpMessage::createClientMessage(
                           method == "SUB" ? Command::Subscribe :
@@ -47,15 +46,15 @@ class FileServerRestBackend : public RestBackend<Mode, VirtualFS, Roles...> {
 
         // For the time being, just use ';' as frame separator. Not meant
         // to be a safe long-term solution:
-        auto currentBegin = serialized.cbegin();
-        const auto bodyEnd = serialized.cend();
-        auto currentEnd = std::find(currentBegin, serialized.cend(), ';');
+        auto       currentBegin = serialized.cbegin();
+        const auto bodyEnd      = serialized.cend();
+        auto       currentEnd   = std::find(currentBegin, serialized.cend(), ';');
 
         for (std::size_t i = 2; i < result.requiredFrameCount(); ++i) {
             result.setFrameData(i, std::string_view(currentBegin, currentEnd),
-                                MessageFrame::dynamic_bytes_tag{});
+                    MessageFrame::dynamic_bytes_tag{});
             currentBegin = (currentEnd != bodyEnd) ? currentEnd + 1 : bodyEnd;
-            currentEnd = std::find(currentBegin, serialized.cend(), ';');
+            currentEnd   = std::find(currentBegin, serialized.cend(), ';');
         }
         return result;
     }
@@ -64,13 +63,13 @@ class FileServerRestBackend : public RestBackend<Mode, VirtualFS, Roles...> {
         _svr.set_mount_point("/", _serverRoot.string());
 
         _svr.Post("/stdio.html",
-        [](const httplib::Request &request, httplib::Response &response) {
-            opencmw::debug::log() << "QtWASM:" << request.body;
-            response.set_content("", "text/plain");
-        });
+                [](const httplib::Request &request, httplib::Response &response) {
+                    opencmw::debug::log() << "QtWASM:" << request.body;
+                    response.set_content("", "text/plain");
+                });
 
         auto cmrcHandler = [this](const httplib::Request &request,
-                                  httplib::Response &response) {
+                                   httplib::Response     &response) {
             if (super_t::_vfs.is_file(request.path)) {
                 auto file = super_t::_vfs.open(request.path);
                 response.set_content(std::string(file.begin(), file.end()), "");
@@ -85,8 +84,8 @@ class FileServerRestBackend : public RestBackend<Mode, VirtualFS, Roles...> {
 };
 
 struct TestContext {
-    opencmw::TimingCtx ctx;
-    std::string testFilter;
+    opencmw::TimingCtx      ctx;
+    std::string             testFilter;
     opencmw::MIME::MimeType contentType = opencmw::MIME::BINARY;
 };
 
@@ -97,9 +96,9 @@ struct TestContext {
 ENABLE_REFLECTION_FOR(TestContext, ctx, testFilter, contentType)
 
 struct Request {
-    std::string name;
-    opencmw::TimingCtx timingCtx;
-    std::string customFilter;
+    std::string             name;
+    opencmw::TimingCtx      timingCtx;
+    std::string             customFilter;
     opencmw::MIME::MimeType contentType = opencmw::MIME::BINARY;
 };
 
@@ -116,21 +115,21 @@ struct Reply {
         REPLY_OPTION4
     };
     */
-    std::string name;
-    bool booleanReturnType;
-    int8_t byteReturnType;
-    int16_t shortReturnType;
-    int32_t intReturnType;
-    int64_t longReturnType;
-    std::string byteArray;
+    std::string        name;
+    bool               booleanReturnType;
+    int8_t             byteReturnType;
+    int16_t            shortReturnType;
+    int32_t            intReturnType;
+    int64_t            longReturnType;
+    std::string        byteArray;
     opencmw::TimingCtx timingCtx;
-    std::string lsaContext;
+    std::string        lsaContext;
     // Option replyOption = Option::REPLY_OPTION2;
 };
 
 ENABLE_REFLECTION_FOR(Reply, name, booleanReturnType, byteReturnType,
-                      shortReturnType, intReturnType, longReturnType, timingCtx,
-                      lsaContext /*, replyOption*/)
+        shortReturnType, intReturnType, longReturnType, timingCtx,
+        lsaContext /*, replyOption*/)
 
 struct CounterData {
     int value;
@@ -148,30 +147,28 @@ std::string_view stripStart(std::string_view s, std::string_view prefix) {
 
 using opencmw::majordomo::Empty;
 
-template <units::basic_fixed_string serviceName, typename... Meta>
+template<units::basic_fixed_string serviceName, typename... Meta>
 class CounterWorker
     : public Worker<serviceName, TestContext, Empty, CounterData, Meta...> {
-    std::atomic<bool> shutdownRequested;
-    std::jthread notifyThread;
-    int counter_value = 0;
-    int counter = 0;
+    std::atomic<bool>     shutdownRequested;
+    std::jthread          notifyThread;
+    int                   counter_value = 0;
+    int                   counter       = 0;
 
     static constexpr auto PROPERTY_NAME = std::string_view("testCounter");
 
 public:
-    using super_t = 
-        Worker<serviceName, TestContext, Empty, CounterData, Meta...>;
+    using super_t = Worker<serviceName, TestContext, Empty, CounterData, Meta...>;
 
-    template <typename BrokerType>
+    template<typename BrokerType>
     explicit CounterWorker(const BrokerType &broker,
-                           std::chrono::milliseconds updateInterval)
+            std::chrono::milliseconds        updateInterval)
         : super_t(broker, {}) {
-
         notifyThread = std::jthread([this, updateInterval] {
             while (!shutdownRequested) {
                 std::this_thread::sleep_for(updateInterval);
                 counter_value = ++counter_value % 100;
-                counter = ++counter;
+                counter       = ++counter;
                 TestContext context;
                 context.contentType = opencmw::MIME::JSON;
                 CounterData reply;
@@ -182,16 +179,15 @@ public:
         });
 
         super_t::setCallback([this](RequestContext &rawCtx, const TestContext &,
-                                    const Empty &, TestContext &,
-                                    CounterData &out) {
+                                     const Empty &, TestContext &,
+                                     CounterData &out) {
             using namespace opencmw;
-            const auto topicPath =
-                URI<RELAXED>(std::string(rawCtx.request.topic()))
-                    .path()
-                    .value_or("");
+            const auto topicPath = URI<RELAXED>(std::string(rawCtx.request.topic()))
+                                           .path()
+                                           .value_or("");
             const auto path = stripStart(topicPath, "/");
-            out.value = counter_value;
-            out.count = counter;
+            out.value       = counter_value;
+            out.count       = counter;
         });
     }
 
@@ -207,14 +203,13 @@ int main() {
     // note: inconsistency: brokerName as ctor argument, worker's serviceName as
     // NTTP note: default roles different from java (has: ADMIN, READ_WRITE,
     // READ_ONLY, ANYONE, NULL)
-    Broker primaryBroker("PrimaryBroker");
-    auto fs = cmrc::assets::get_filesystem();
+    Broker                                          primaryBroker("PrimaryBroker");
+    auto                                            fs = cmrc::assets::get_filesystem();
 
-    FileServerRestBackend<PLAIN_HTTP, decltype(fs)> rest(primaryBroker, fs, 
-                                                         "./");
+    FileServerRestBackend<PLAIN_HTTP, decltype(fs)> rest(primaryBroker, fs,
+            "./");
 
-    const auto brokerRouterAddress =
-        primaryBroker.bind(URI<>("mds://127.0.0.1:12345"));
+    const auto                                      brokerRouterAddress = primaryBroker.bind(URI<>("mds://127.0.0.1:12345"));
 
     if (!brokerRouterAddress) {
         std::cerr << "Could not bind to broker address" << std::endl;
@@ -225,23 +220,23 @@ int main() {
     std::jthread primaryBrokerThread([&primaryBroker] { primaryBroker.run(); });
 
     // second broker to test DNS functionalities
-    Broker secondaryBroker("SecondaryTestBroker",
-                           {.dnsAddress = brokerRouterAddress->str()});
+    Broker       secondaryBroker("SecondaryTestBroker",
+                  { .dnsAddress = brokerRouterAddress->str() });
 
     std::jthread secondaryBrokerThread(
-        [&secondaryBroker] { secondaryBroker.run(); });
+            [&secondaryBroker] { secondaryBroker.run(); });
 
     // TODO IIRC we agreed that service names should be valid URIs and thus have
     // a / prepended, but "/helloWorld" doesn't work with the REST interface
-    // (http://localhost:8080/helloWorld and http://localhost:8080//helloWorld 
+    // (http://localhost:8080/helloWorld and http://localhost:8080//helloWorld
     // are mapped to "helloWorld")
 
     // TODO '"Reply": { "name": ... }' isn't valid json I think (not an object
-    // at top-level; also, Firefox doesn't like it). Should we omit the 
+    // at top-level; also, Firefox doesn't like it). Should we omit the
     // '"Reply:"?
 
     CounterWorker<"testCounter", description<"Returns counter value">>
-        counterWorker(primaryBroker, std::chrono::seconds(2));
+                 counterWorker(primaryBroker, std::chrono::seconds(2));
 
     std::jthread counterThread([&counterWorker] { counterWorker.run(); });
 
