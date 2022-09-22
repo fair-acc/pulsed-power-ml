a3630da3e672619b2546d98ece0712e1bad552d8
Sinisa Falamic <sinisa.falamic@infoteam.de>
2022-09-22 16:20:08 +0200

    implements multipoller WIP, channelNames consist of the signal name and sample rate

--- src/opencmw_worker/cmake/Dependencies.cmake
+++ src/opencmw_worker/cmake/Dependencies.cmake
@@ -1,5 +1,13 @@
 include(FetchContent)
 
+set(FETCHCONTENT_QUIET FALSE)
+FetchContent_Declare(
+        cpp-httplib
+        GIT_REPOSITORY https://github.com/yhirose/cpp-httplib.git
+        GIT_TAG v0.11.2 # latest v0.11.2
+)
+FetchContent_MakeAvailable(cpp-httplib)
+
 FetchContent_Declare(
         opencmw-cpp
         GIT_REPOSITORY https://github.com/fair-acc/opencmw-cpp.git
