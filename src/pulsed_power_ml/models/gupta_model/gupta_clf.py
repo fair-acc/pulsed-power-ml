1b5a41c27af6f2cb1363b886212d08a2e110a4bd
Dr. Thomas Kittler <thomas.kittler@infoteam.de>
2022-11-17 17:59:48 +0100

    feat: Implement switching_offset into predict method

--- src/pulsed_power_ml/models/gupta_model/gupta_clf.py
+++ src/pulsed_power_ml/models/gupta_model/gupta_clf.py
@@ -24,6 +24,7 @@ class GuptaClassifier(BaseEstimator, ClassifierMixin):
                  sample_rate: int = 2_000_000,
                  n_known_appliances: int = 10,
                  spectrum_type: int = 0,
+                 switching_offset: int = 1,
                  n_peaks_max: int = 10,
                  apparent_power_list: list = [],
                  n_neighbors: int = 5,
@@ -45,6 +46,8 @@ class GuptaClassifier(BaseEstimator, ClassifierMixin):
             Number of known appliances.
         spectrum_type
             Determines which spectrum is used. 0: voltage, 1: current, 2: apparent power
+        switching_offset
+            Number of frames to skip after a switching event has been detected before classifying the event.
         n_peaks_max
             Max. number of peaks, which are used to calculate features.
         apparent_power_list
@@ -69,7 +72,8 @@ class GuptaClassifier(BaseEstimator, ClassifierMixin):
         self.n_neighbors = n_neighbors
         self.knn_weights = knn_weights
         self.distance_threshold = distance_threshold
-        self.clf = neighbors.KNeighborsClassifier()
+        self.clf = neighbors.KNeighborsClassifier(n_neighbors=self.n_neighbors,
+                                                  weights=self.knn_weights)
 
         # Containers
         self.current_state_vector = np.zeros(n_known_appliances + 1)  # one additional for "others"
@@ -77,6 +81,11 @@ class GuptaClassifier(BaseEstimator, ClassifierMixin):
 
         # Apparent Power data base
         self.apparent_power_list = apparent_power_list
+
+        # Class attributes to accommodate for disturbances due to mechanical switches
+        self.switching_offset = switching_offset
+        self.n_data_points_skipped = 0
+        self.skip_data_point = False
 
         return
 
@@ -112,16 +121,45 @@ class GuptaClassifier(BaseEstimator, ClassifierMixin):
         y
             state_vector containing the current estimated power for each appliance.
         """
+        # If a switching event has been detected and self.switching_offset is not 0, skip frames before attempting
+        # a classification
+        if self.skip_data_point is True:
+            # Not enough data points have been skipped yet
+            if self.n_data_points_skipped < self.switching_offset:
+                self.n_data_points_skipped += 1
+                return self.current_state_vector
+            # Enough data points have been skipped, do the classification now
+            else:
+                spectrum, apparent_power = self.crop_data_point(X)
+
+                # calculate mean background
+                current_background = calculate_background(np.array(self.background_vector))
+
+                # Get cleaned spectrum
+                cleaned_spectrum = subtract_background(raw_spectrum=spectrum,
+                                                       background=current_background)
+
+                # Classify
+                self.current_state_vector = self.classify_switching_event(cleaned_spectrum=cleaned_spectrum,
+                                                                          current_apparent_power=apparent_power)
+
+                # clear background vector
+                self.background_vector.clear()
+
+                # reset skip flag
+                self.skip_data_point = False
+
+                # return new state vector
+                return self.current_state_vector
+
         # 0. Step: Remove unused information in data point
         spectrum, apparent_power = self.crop_data_point(X)
-
-        # 0.1 Step: Correct spectrum (ToDo: Remove this step once training data w/o log_10 amplitudes are available)
-        spectrum = 10**spectrum
 
         # 1. Step: Check if background vector is full
         if self.background_n > len(self.background_vector):
             # Add current data point to background vector and return last state vector
             self.background_vector.append(spectrum)
+            self.current_state_vector = self.calculate_unknown_apparent_power(current_apparent_power=apparent_power)
             return self.current_state_vector
 
         # 2. Calculate background
@@ -137,34 +175,25 @@ class GuptaClassifier(BaseEstimator, ClassifierMixin):
         if True not in event_detected_flag:
             # add spectrum to background vector and return last state vector
             self.background_vector.append(spectrum)
+            self.current_state_vector = self.calculate_unknown_apparent_power(current_apparent_power=apparent_power)
             return self.current_state_vector
 
-        # 5. Calculate feature vector
-        print("Event detected: Calculate feature vector")
-        feature_vector = calculate_feature_vector(cleaned_spectrum=cleaned_spectrum,
-                                                  n_peaks_max=self.n_peaks_max,
-                                                  fft_size_real=self.fft_size_real,
-                                                  sample_rate=self.sample_rate)
+        # 5. If self.switching_offset is 0, then use the current data point to classify the event
+        if self.switching_offset == 0:
+            self.current_state_vector = self.classify_switching_event(cleaned_spectrum=cleaned_spectrum,
+                                                                      current_apparent_power=apparent_power)
+            # clear background vector
+            self.background_vector.clear()
 
-        # 6. Empty background vector
-        self.background_vector.clear()
+            # return new state vector
+            return self.current_state_vector
 
-        # 7. Classify event
-        distances, _ = self.clf.kneighbors([feature_vector])
-
-        # Check if known or unknown event via the smallest distance
-        if distances.min() > self.distance_threshold:
-            # Case 1: Unknown event
-            event_class = np.zeros(self.n_known_appliances * 2 + 1)[-1] = 1
+        # If self.switching_offset is not 0, a number of data points needs to be skipped before a classification
         else:
-            # Case 2: Known event
-            event_class = self.clf.predict([feature_vector])
-
-        # 8. Update current state vector accordingly
-        self.current_state_vector = self.update_state_vector(event_class=event_class,
-                                                             current_apparent_power=apparent_power)
-
-        return self.current_state_vector
+            self.skip_data_point = True
+            self.n_data_points_skipped += 1
+            # return the current state vector until the classification is done
+            return self.current_state_vector
 
     def crop_data_point(self, X: np.array) -> (np.array, int):
         """
@@ -187,9 +216,8 @@ class GuptaClassifier(BaseEstimator, ClassifierMixin):
         apparent_power = X[-2]
         return spectrum, apparent_power
 
-    def update_state_vector(self,
-                            event_class: np.array,
-                            current_apparent_power: float) -> np.array:
+    def calculate_state_vector(self,
+                               event_class: np.array) -> np.array:
         """
         Given the current state vector and a classification results, return an updated state vector.
 
@@ -197,8 +225,6 @@ class GuptaClassifier(BaseEstimator, ClassifierMixin):
         ----------
         event_class
             One-hot encoded array of length 2N+1 (N is the number of known appliances).
-        current_apparent_power
-            Most recent value of the apparent power
 
         Returns
         -------
@@ -207,22 +233,79 @@ class GuptaClassifier(BaseEstimator, ClassifierMixin):
 
         updated_state_vector = copy.deepcopy(self.current_state_vector)
 
-        if np.argmax(event_class) <= self.n_known_appliances:
+        if np.argmax(event_class) < self.n_known_appliances:
             # Case 1: Known appliance is switched on
             state_vector_index = np.argmax(event_class)
+            print(state_vector_index)
             updated_state_vector[state_vector_index] = self.apparent_power_list[state_vector_index][1]
 
-        elif self.n_known_appliances < np.argmax(event_class) <= self.n_known_appliances:
+        elif self.n_known_appliances <= np.argmax(event_class) < self.n_known_appliances * 2:
             # Case 2: Known appliance is switched off
-            state_vector_index = int(np.argmax(event_class) / 2)
+            state_vector_index = int(np.argmax(event_class) - self.n_known_appliances)
             updated_state_vector[state_vector_index] = 0
 
         else:
             # Case 3: Unknown event
             pass
 
-        # Update current state vector's power value for "unknown"
-        known_power = sum(updated_state_vector[:-1])
-        updated_state_vector[-1] = max(current_apparent_power - known_power, 0)
-
         return updated_state_vector
+
+    def calculate_unknown_apparent_power(self, current_apparent_power: float) -> np.array:
+        """
+        Update the value of "unknown" in the state vector.
+
+        Parameters
+        ----------
+        current_apparent_power
+            Current, total value of apparent power
+
+        Returns
+        -------
+        updated_state_vector
+        """
+        updated_state_vector = copy.deepcopy(self.current_state_vector)
+        known_power = sum(self.current_state_vector[:-1])
+        updated_state_vector[-1] = max(current_apparent_power - known_power, 0)
+        return updated_state_vector
+
+    def classify_switching_event(self,
+                                 cleaned_spectrum: np.array,
+                                 current_apparent_power: float) -> np.array:
+        """
+        Classify a switching event based on the cleaned spectrum.
+
+        Parameters
+        ----------
+        cleaned_spectrum
+            Array containing the cleaned spectrum.
+        current_apparent_power
+            Total of the apparent power.
+        Returns
+        -------
+        updated_state_vector
+        """
+        # 1. Calculate feature vector
+        print("Event detected: Calculate feature vector")
+        feature_vector = calculate_feature_vector(cleaned_spectrum=cleaned_spectrum,
+                                                  n_peaks_max=self.n_peaks_max,
+                                                  fft_size_real=self.fft_size_real,
+                                                  sample_rate=self.sample_rate)
+
+        # 2. Classify event
+        distances, _ = self.clf.kneighbors([feature_vector])
+
+        # Check if known or unknown event via the smallest distance
+        if distances.min() > self.distance_threshold:
+            # Case 1: Unknown event
+            event_class = np.zeros(self.n_known_appliances * 2 + 1)[-1] = 1
+        else:
+            # Case 2: Known event
+            event_class = self.clf.predict([feature_vector])
+
+        # 3. Update current state vector accordingly
+        self.current_state_vector = self.calculate_state_vector(event_class=event_class)
+
+        # 4. Update current state vector's power value for "unknown"
+        self.current_state_vector = self.calculate_unknown_apparent_power(current_apparent_power=current_apparent_power)
+
+        return self.current_state_vector
