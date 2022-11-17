010c323926f429145610c0e8cef9a8dd2923f199
Dr. Thomas Kittler <92721368+ThomasKittler@users.noreply.github.com>
2022-11-17 13:19:39 +0100

    Nilm feature initial framework structure (#91)
    * feat: First draft of read_training_data function
    
    * feat: add viasualizations for training data
    
    * feat: add parameter file, background calculation
    
    * feat: Add titles to subplots
    
    refactor: Include line break to keep max. line width
    
    * feat: add switch and event detection functions
    
    * feat: Add functions to transform training data into list of data points
    
    * feat: Implement the Gupta classifier class
    
    The implementation is yet incomplete, thus untested.
    
    * feat: Implement calculate_feature_vector function
    
    * feat: Add function to calculate frequqnecy from spectrum
    
    * fix: update switch detection, gupta thresh. method
    
    * feat: Implement apparent power db functions and update_state_vector
    
    * feat: add function to label training data
    
    * feat: Integrate KNN Classifier into GuptaModel class
    
    * feat: extended labelling functions
    
    * fix: remove unneccessary demo function
    
    * fix: adapt labelling functions to correct shapes
    
    * feat: Add test notebook for gupta model
    
    feat: Add max apparent power values for tube, halo, led to power db
    
    * fix: rename sample_size to sample_rate
    
    * feat: Implement first try w/ ONNX
    
    * feat: write output csv for training data labelling
    
    * feat: Change colormap for contour plots, add first ONNX tests
    
    * fix: Include switch_detected changes in GuptaClassifier.predict
    
    refactor: minor code clean up
    
    Co-authored-by: Maria Hirsch <maria.hirsch@infoteam.de>

---
+++ src/pulsed_power_ml/model_framework/training_data_labelling.py
@@ -0,0 +1,265 @@
+import numpy as np
+from glob import glob
+from src.pulsed_power_ml.model_framework.data_io import load_fft_file
+from src.pulsed_power_ml.models.gupta_model.gupta_utils import calculate_background, read_parameters, subtract_background, switch_detected, update_background_vector, calculate_feature_vector
+
+def trainingdata_switch_detector(spectra: np.ndarray, parameters: dict):
+    """
+    Function to automatically label training spectra (from GNU Radio).
+
+    Parameters
+    ----------
+    spectra
+        array of raw spectra.
+    parameters
+        Dictionary of input parameters, containing 'appliances' and 'threshold'.
+
+    Returns
+    -------
+    switch_positions
+        Array of switch_positions for switch on and switch off.
+    """
+    switch_positions=np.zeros((2,spectra.__len__()))
+    
+    background_vector = []
+    current_background = None
+    counter = 0
+    for spectrum in spectra:
+        spectrum = 10**spectrum
+
+        # is there a background?
+        if current_background is not None:
+            residual = subtract_background(spectrum, current_background)
+            switch = switch_detected(residual, parameters['threshold'])
+            if True in switch:
+                switch_positions[switch.index(True),counter]=1
+                print ("switch detected")
+                print(counter)
+                background_vector = []
+                current_background = None
+            else:
+                background_vector = update_background_vector(background_vector, spectrum)
+                current_background = calculate_background(background_vector)
+        else:
+            if background_vector.__len__() == 0:
+                background_vector = spectrum.reshape(1,-1)
+            else:
+                background_vector = np.vstack((background_vector, spectrum))
+
+            # is there enough data to calculate background?
+            if background_vector.__len__() == parameters['background_n']:
+                current_background = calculate_background(background_vector)
+                print("background calculated")
+                print(counter)
+        
+        counter+=1
+    return switch_positions
+
+
+def get_switch_features(spectra, switch_positions, parameters):
+    """
+    Function to get clean spectra for switch events. Please make sure that there are at least 26 regular spectra for background calculation 
+
+    Parameters
+    ----------
+    spectra
+        array of raw spectra.
+    switch_positions
+        Switch positions as returned by the trainingdata_switch detector.
+    parameters
+        Dictionary of input parameters, containing 'appliances' and 'threshold'.
+
+    Returns
+    -------
+    switch_features
+        Array of feature vectors for each switch event marked in switch_positions.
+    """
+
+    switch_indices = np.where(switch_positions==1)[1]
+    switch_features = []
+    for ind in switch_indices:
+        raw_spectra = spectra[ind-parameters['background_n']-1:ind]
+        background_vector = []
+        for spectrum in raw_spectra:
+            spectrum = 10**spectrum
+            if background_vector.__len__() == 0:
+                background_vector = spectrum.reshape(1,-1)
+            else:
+                background_vector = np.vstack((background_vector, spectrum))
+        
+        current_background = calculate_background(background_vector)
+        
+        raw_switch_spectrum = spectra[ind+parameters["switching_offset"]]
+        raw_switch_spectrum = 10**raw_switch_spectrum
+        clean_switch_spectrum = subtract_background(raw_switch_spectrum, current_background)
+        if switch_features.__len__() == 0:
+            switch_features = calculate_feature_vector(clean_switch_spectrum, 
+                                                       parameters['n_peaks'], 
+                                                       parameters['fft_size']/2,
+                                                       parameters['sample_rate']).reshape(1,-1)
+        else:
+            switch_features = np.vstack((switch_features, calculate_feature_vector(clean_switch_spectrum,
+                                                                                   parameters['n_peaks'], 
+                                                                                   parameters['fft_size']/2,
+                                                                                   parameters['sample_rate'])))
+            
+    return switch_features
+        
+
+
+def make_labeled_training_data(training_file: str, parameter_file: str):
+    """
+    Function to get clean spectra and respective labels. 
+
+    Parameters
+    ----------
+    training_file
+        Path to the binary containing the spectrum.
+    parameter_file
+        Path to the yml file containing the parameters.
+
+    Returns
+    -------
+    switch_features
+        Array of clean switch_features for each switch event marked in switch_positions.
+    labels
+        Array of labels, labels[0] referring to switch-on, labels[1] referring to switch-off events.
+    """
+    # 1 load specturm array
+    pars = read_parameters(parameter_file)
+    spectra = load_fft_file(training_file, pars["fft_size"])
+    # detect switiching events
+    switch_positions = trainingdata_switch_detector(spectra,pars)
+    # disect spectrum during switching event 
+    switch_features = get_switch_features(spectra,switch_positions,pars)
+    # add label 
+    events = np.where(switch_positions==1)[0] # 0 = on, 1 = off
+    labels = np.zeros((switch_features.__len__(),2))
+    counter = 0
+    for event in events:
+        if event == 0:
+            labels[counter,0] = 1
+        else:
+            labels[counter,1] = 1
+        counter += 1
+    return [switch_features, labels]
+
+
+def explode_to_complete_label_vector(labels: np.ndarray, appliance_id: int, parameter_file: str):
+    """
+    Function to explode single appliance label vector to complete label vector size of form
+    [appliance1_on, appliance1_off, appliance2_on, appliance2_off,....]
+
+    Parameters
+    ----------
+    labels
+        Array of single appliance labels, array[0] containing switch-on, array[1] switch_off events
+    appliance
+        Appliance name, matching name given in parameter file.
+    parameter_file
+        Path to the yml file containing the parameters.
+        
+    Returns
+    -------
+    complete_labels
+        Array of labels including all appliances given in parameter file.
+    """
+    # ToDo: Make appliance_id list-compatible for mixed spectra
+    
+    # get index of single appliance in parameter file appliances list
+    pars = read_parameters(parameter_file)
+    ind = appliance_id-1
+    
+    # construct zeroes array of total size 
+    complete_label_vector = np.zeros((labels.__len__(), (pars["appliances"].__len__()*2)+1))
+    
+    # replace respective entries with input labels
+    counter = 0
+    for label in labels:
+        if label[0]==1:
+            complete_label_vector[counter][ind] = 1
+        elif label[1]==1:
+            complete_label_vector[counter][ind+pars['appliances'].__len__()] = 1
+        counter += 1
+    
+    return complete_label_vector
+
+
+def write_training_data_csvs(path_to_training_data_folders: str, output_path: str, parameter_file: str):
+    """
+    Function to write csvs containing training features and respective labels. 
+
+    Parameters
+    ----------
+    path_to_training_data_folders
+        Path to the folder containing the subfolders for all the binary files of one date
+    output_path
+        Path to folder where csvs will be written to
+    parameter_file
+        Path to the yml file containing the parameters.
+
+    Returns
+    -------
+    1 if run without errors
+    """
+    pars = read_parameters(parameter_file)
+    sorts = ["ApparentPower","Voltage","Current"]
+    appliances = list()
+    appliance_ids = list()
+    for item in pars['appliances']:
+        appliances.append(list(item.values())[0])
+        appliance_ids.append(list(item.keys())[0])
+    # for each sort of spectrum (ApparentPower, Current, Voltage files)
+    # make list of files of all appliances (glob)
+    # loop over appliances to make features and labels
+    for sort in sorts:
+        filepath = '{0}/*/*{1}*'.format(path_to_training_data_folders, sort)
+        files = glob(filepath)
+        allfeatures = []
+        alllabels = []
+        for file in files: # loop over appliances
+            if any(sort in file for sort in sorts):
+                s = sorts[np.where([sort in file for sort in sorts])[0][0]]
+            appliance = None
+            appliance_id = None
+            if any(appl in file for appl in appliances):
+                appliance = appliances[np.where([appliance in file for appliance in appliances])[0][0]]
+                appliance_id = appliance_ids[appliances.index(appliance)]
+            if appliance is not None: # single appliance, not mixed
+                features, labels = make_labeled_training_data(file, parameter_file)
+                if features.__len__() == 0:
+                    print("Warning: No switches found for "+ appliance + " in " + s + "!")
+                else:
+                    compl_labels = explode_to_complete_label_vector(labels,appliance_id,parameter_file)
+                    print(features.__len__())
+                    if allfeatures.__len__() == 0:
+                        allfeatures = features
+                        alllabels = compl_labels
+                    else:
+                        allfeatures = np.vstack((allfeatures, features))
+                        alllabels = np.vstack((alllabels, compl_labels))    
+
+        np.savetxt(output_path + "Features_" + s + ".csv", allfeatures, delimiter=",")
+        np.savetxt(output_path + "Labels_" + s + ".csv", alllabels, delimiter=",")
+        
+    return(1)
+    
+
+if __name__ == "__main__":
+    # pars = read_parameters("src/pulsed_power_ml/models/gupta_model/parameters.yml")
+    # print(pars)
+    # spectra = load_fft_file("../training_data/2022-10-25_training_data/led/FFTApparentPower_LEDOnOff_FFTSize131072",
+    #                     2**17)
+    
+    # switch_positions = trainingdata_switch_detector(spectra,pars)
+    # print(np.where(switch_positions==1))
+    
+    # training_file = "../training_data/2022-10-25_training_data/led/FFTCurrent_ledOnOff_FFTSize131072"
+    # parameter_file = "src/pulsed_power_ml/models/gupta_model/parameters.yml"
+    # features, labels = make_labeled_training_data(training_file, parameter_file)
+    # compl_labels = explode_to_complete_label_vector(labels,1,parameter_file)
+    
+    path_to_training_data_folders = "../training_data/2022-10-25_training_data/"
+    output_path = "../training_data/labels_20221025/"
+    parameter_file = "src/pulsed_power_ml/models/gupta_model/parameters.yml"
+    _ = write_training_data_csvs(path_to_training_data_folders,output_path,parameter_file)
---
+++ src/pulsed_power_ml/models/gupta_model/gupta_utils.py
@@ -0,0 +1,310 @@
+"""
+This module contains functions needed by the Gupta classification algorithm.
+"""
+from typing import Tuple
+import yaml
+from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+from scipy.optimize import curve_fit
+
+from src.pulsed_power_ml.model_framework.data_io import load_fft_file
+
+
+def read_parameters(parameter_path: str):
+    """
+    Read parameters from a parameter yml file into a dictionary.
+
+    Parameters
+    ----------
+    parameter_path
+        Path to the .yml file.
+
+    Returns
+    -------
+    Dictionary of parameters.
+    """
+    param_dic = yaml.safe_load(Path(parameter_path).read_text())
+    param_dic['sec_per_fft'] = param_dic['fft_size'] / param_dic['sample_rate']
+    param_dic['freq_per_bin'] = param_dic['sample_rate'] / param_dic['fft_size']
+    return param_dic
+
+
+def read_power_data_base(path_to_file: str) -> list:
+    """
+    Read yaml file containing the apparent power values of all known devices and return a list in this form:
+    [(name_0, value_0), (name_1, value_1), ...]
+
+    Parameters
+    ----------
+    path_to_file
+        Path to yaml file
+
+    Returns
+    -------
+    apparent_power_list
+    """
+    power_dict = yaml.safe_load(Path(path_to_file).read_text())
+    apparent_power_list = list()
+    for key, value in power_dict.items():
+        apparent_power_list.append((key, value))
+    return apparent_power_list
+
+
+def calculate_background(background_points: np.ndarray):
+    """
+    Calculates background from input spectra.
+
+    Parameters
+    ----------
+    background_points
+        2D array of spectra to calculate background from.
+
+    Returns
+    -------
+    Background spectrum.
+    """    
+    background = background_points.mean(axis=0)
+    return background
+
+
+def subtract_background(raw_spectrum: np.ndarray, background: np.ndarray):
+    """
+    Subtracts background from spectrum.
+
+    Parameters
+    ----------
+    raw_spectrum
+        Raw spectrum.
+    background
+        Background to be subtracted from raw spectrum -- needs to have same length as spectrum.
+
+    Returns
+    -------
+    Background subtracted spectrum
+    """
+    return raw_spectrum-background
+
+
+def switch_detected(res_spectrum: np.ndarray, threshold: np.int) -> Tuple[bool, bool]:
+    """
+    Scans background subtracted spectrum for switch event 
+    (signal larger than input parameterthreshold value) 
+    to avoid dead time because of background re-calculation.
+
+    Parameters
+    ----------
+    res_spectrum
+        Background subtracted spectrum.
+    threshold
+        Threshold value
+
+    Returns
+    -------
+    flag_tuple
+        Tuple containing two boolean values, first is True, if a "switch on" event is detected, second is True if
+        a "switch off" event ist detected.
+    """
+
+    # how many bins are above the standard deviation?
+    n_bins_above_thr = res_spectrum[res_spectrum > threshold].__len__()
+
+    # how many bins are below the negative standard deviation?
+    n_bins_below_minus_thr = res_spectrum[res_spectrum < -threshold].__len__()
+
+    switchon = False
+    switchoff = False
+    
+    if n_bins_above_thr >= 1:
+        switchon = True
+    elif n_bins_below_minus_thr >= 1:
+        switchoff = True
+
+    return switchon, switchoff
+    
+
+def event_detected(res_spectrum: np.ndarray):
+    """
+    Scans background subtracted spectrum for peaks. 
+    Threshold for peak detection is more than one peak above 4 times the standard deviation.
+
+    Parameters
+    ----------
+    res_spectrum
+        Background subtracted spectrum.
+
+    Returns
+    -------
+    Boolean: True if peak was detected, false if no peak was detected
+    """
+    
+    # ToDo: equivalent loop with negative signal to find dips (switch off)
+    
+    peaks, _ = find_peaks(res_spectrum, height=4*res_spectrum.std())
+    negpeaks, __ = find_peaks(-1*res_spectrum, height=4*res_spectrum.std())
+
+    if peaks.__len__() > 1:
+        peak_detected = True
+    elif negpeaks.__len__() > 1:
+        peak_detected = True
+    else:
+        peak_detected = False
+
+    return peak_detected
+
+
+def update_background_vector(old_background_vector: np.ndarray, raw_spectrum: np.ndarray):
+    """
+    Updates an existing background vector by removing the first element 
+    and appending the input spectrum at the end.
+    
+    Parameters
+    ----------
+    old_background_vector
+        Vector of n raw spectra containing x bins each.
+    raw_spectrum
+        Raw spectrum of length x to be added to the vector.
+
+    Returns
+    -------
+    np.ndarray((n,x)): updated background_vector
+    """
+    new_background_vector = np.vstack((old_background_vector[1:], raw_spectrum))
+    return new_background_vector
+
+
+def gaussian(x: float, a: float, mu: float, sigma: float) -> float:
+    """
+    Gaussian with amplitude.
+
+    Parameters
+    ----------
+    x
+        Independent variable.
+    a
+        Amplitude
+    mu
+        Expected value
+    sigma
+        Sqrt(Variance)
+
+    Returns
+    -------
+    a * exp(- (x - mu)**2 / (2 * sigma**2) )
+    """
+    return a * np.exp(-(x - mu)**2 / (2 * sigma**2))
+
+
+def fit_gaussian_to_peak(frequency_window: np.array, magnitude_window: np.array) -> np.array:
+    """
+    Fit a gaussian to the data points in **peak_window**
+
+    Parameters
+    ----------
+    frequency_window
+        Array of frequencies (x-values).
+    magnitude_window
+        Array of corresponding magnitudes (y-values)
+
+    Returns
+    -------
+    parameters
+        Array with [a, mu, sigma]
+    """
+    # Initial guesses
+    a_init = magnitude_window.max()
+    mu_init = frequency_window.mean()
+    sigma_init = np.sqrt(sum(magnitude_window * (frequency_window - mu_init)**2) / sum(magnitude_window))
+
+    popt, pcov = curve_fit(f=gaussian,
+                           xdata=frequency_window,
+                           ydata=magnitude_window,
+                           p0=[a_init, mu_init, sigma_init],
+                           full_output=False)
+    return popt
+
+
+def calculate_feature_vector(cleaned_spectrum: np.array,
+                             n_peaks_max: int,
+                             fft_size_real: int,
+                             sample_rate: int) -> np.array:
+    """
+    Calculate a feature vector given a cleaned spectrum.
+
+    Parameters
+    ----------
+    cleaned_spectrum
+        Array. Spectrum with background removed.
+    n_peaks_max
+        Max. number of peaks that are used to calculate features.
+    fft_size_real
+        Number of points in the real part of the spectrum.
+    sample_rate
+        Sample rate of the DAQ.
+
+    Returns
+    -------
+    feature_vector
+        Array of length 3 * n_peaks_max of the form: [a_0, mu_0, sigma_0, a_1, mu_1, sigma_1, ...]
+    """
+    # get peaks
+    min_peak_height = 4 * cleaned_spectrum.std()  # This could probably be optimized.
+    if abs(cleaned_spectrum.min()) > abs(cleaned_spectrum.max()):
+        switch_off_factor = -1
+    else:
+        switch_off_factor = 1
+    peak_indices, peak_properties = find_peaks(x=cleaned_spectrum * switch_off_factor,
+                                               height=min_peak_height)
+
+    # remove peaks in the first and last bin of the spectrum (cannot be used for gaussian fit)
+    peak_height_list = [(peak_index, peak_height)
+                        for peak_index, peak_height in zip(peak_indices, peak_properties['peak_heights'])
+                        if peak_index > 0 and peak_index < fft_size_real]
+
+    # select highest peaks if more than n_peaks_max peaks have been found
+    if len(peak_height_list) > n_peaks_max:
+        peak_height_list = sorted(peak_height_list,
+                                  key=lambda x: x[1],
+                                  reverse=True)[:n_peaks_max]
+        peak_height_list = sorted(peak_height_list,
+                                  key=lambda x: x[0])
+
+    # Apply fit and store fit parameters in feature vector
+    freq_per_bin = sample_rate / fft_size_real
+    feature_vector = np.zeros(3 * n_peaks_max)
+    for i, (peak_index, peak_height) in enumerate(peak_height_list):
+        frequencies = [j * freq_per_bin + freq_per_bin / 2 for j in [peak_index - 1, peak_index, peak_index + 1]]
+        magnitudes = cleaned_spectrum[peak_index - 1:peak_index + 2]
+        a, mu, sigma = fit_gaussian_to_peak(np.array(frequencies), np.array(magnitudes))
+        feature_vector[i * 3] = a  # * switch_off_factor For switch off events, amplitudes should be negative
+        feature_vector[i * 3 + 1] = mu
+        feature_vector[i * 3 + 2] = sigma
+
+    return feature_vector
+
+
+if __name__ == "__main__":
+    pars = read_parameters("src/pulsed_power_ml/models/gupta_model/parameters.yml")
+
+    # spectra = load_fft_file("../training_data/training_data_2022-10-12/led/LEDOnOff-FFTApparentPower-200KS-FFTSize16k_DAT_2022-10-12",pars['fft_size'])
+    spectra = load_fft_file("../training_data/training_data_2022-10-12/halo/HaloOnOff-FFTApparentPower-200KS-FFTSize16k_DAT_2022-10-12",pars['fft_size'])
+    # spectra = load_fft_file("../training_data/training_data_2022-10-12/tube/TubeOnOff-FFTApparentPower-200KS-FFTSize16k_DAT_2022-10-12",pars['fft_size'])
+    # spectra = load_fft_file("../training_data/training_data_2022-10-12/mixed/MixedOnOff-FFTApparentPower-200KS-FFTSize16k_DAT_2022-10-12",pars['fft_size'])
+
+    background_vector = spectra[np.arange(-pars['background_n'],0)]
+    current_background = calculate_background(background_vector)
+    
+    plt.xlim([0,250])
+    plt.imshow(spectra-current_background,aspect="auto")
+    plt.show()
+     
+    # for spectrum in spectra[170:176]:  # Einschaltvorgang LED
+    for spectrum in spectra[218:225]:  # Einschaltvorgang Halo
+        spectrum = 10**spectrum
+        residual = subtract_background(spectrum, current_background)
+        # plt.plot(residual)
+        # plt.show()
+        switch = switch_detected(residual,pars['threshold'])
+
+        print("Switch detected: {}".format(True in switch))
