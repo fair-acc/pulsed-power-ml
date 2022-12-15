313179fb64fba05aab15d7c9238d6b4c63a97049
johannaheuser <107842187+johannaheuser@users.noreply.github.com>
2022-12-15 14:38:14 +0100

    Limiting curve (#104)
    * implement GR opencmw_freq_sink and frequency-domain worker
    
    gr-pulsed_power: add opencmw frequency sink as a vector sink
    opencmw_worker: implements get/set handler function for both time-domain and freq-domain worker
    pulsed power service: add FFT calculation to the GR flowgraph and frequency worker thread
    
    * GR flowgraph: add a separate signal source for the opencmw frequency sink
    * properly cast GR multiply_const multiplicative constant parameter
    * Visualize limiting curve via separate worker
    * Reduce sample rate of simulated signals; Remove plot titles; Correct axes descriptions
    * Set x-axis limits for power and frequency plot to time intervals of 5 minutes
    
    Co-authored-by: Sinisa Falamic <sinisa.falamic@infoteam.de>

--- src/implot_visualization/app/plot_tools/plot_tools.h
+++ src/implot_visualization/app/plot_tools/plot_tools.h
@@ -67,7 +67,7 @@ void plotGrSignals(std::vector<ScrollingBuffer> &signals) {
     ImPlot::SetupAxes("UTC Time", "Value", xflags, yflags);
     auto   clock       = std::chrono::system_clock::now();
     double currentTime = static_cast<double>(std::chrono::duration_cast<std::chrono::seconds>(clock.time_since_epoch()).count());
-    ImPlot::SetupAxisLimits(ImAxis_X1, currentTime - 30.0, currentTime, ImGuiCond_Always);
+    ImPlot::SetupAxisLimits(ImAxis_X1, currentTime - 0.06, currentTime, ImGuiCond_Always);
     ImPlot::SetupAxisScale(ImAxis_X1, ImPlotScale_Time);
     ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL, 0.5f);
     Plotter::plotSignals(signals);
@@ -76,8 +76,11 @@ void plotGrSignals(std::vector<ScrollingBuffer> &signals) {
 void plotBandpassFilter(std::vector<ScrollingBuffer> &signals) {
     static ImPlotAxisFlags xflags = ImPlotAxisFlags_None;
     static ImPlotAxisFlags yflags = ImPlotAxisFlags_AutoFit | ImPlotAxisFlags_RangeFit;
-    ImPlot::SetupAxes("time (ms)", "I(A)", xflags, yflags);
-    ImPlot::SetupAxisLimits(ImAxis_X1, 0, 60, ImGuiCond_Always);
+    ImPlot::SetupAxes("UTC Time", "I(A)", xflags, yflags);
+    auto   clock       = std::chrono::system_clock::now();
+    double currentTime = static_cast<double>(std::chrono::duration_cast<std::chrono::seconds>(clock.time_since_epoch()).count());
+    ImPlot::SetupAxisLimits(ImAxis_X1, currentTime - 0.06, currentTime, ImGuiCond_Always);
+    ImPlot::SetupAxisScale(ImAxis_X1, ImPlotScale_Time);
     ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL, 0.5f);
     // plotSignals(signals);
 }
@@ -85,10 +88,10 @@ void plotBandpassFilter(std::vector<ScrollingBuffer> &signals) {
 void plotPower(std::vector<ScrollingBuffer> &signals) {
     static ImPlotAxisFlags xflags = ImPlotAxisFlags_None;
     static ImPlotAxisFlags yflags = ImPlotAxisFlags_AutoFit | ImPlotAxisFlags_RangeFit;
-    ImPlot::SetupAxes("time (s)", "P(W), Q(Var), S(VA)", xflags, yflags);
+    ImPlot::SetupAxes("UTC Time", "P(W), Q(Var), S(VA)", xflags, yflags);
     auto   clock       = std::chrono::system_clock::now();
     double currentTime = static_cast<double>(std::chrono::duration_cast<std::chrono::seconds>(clock.time_since_epoch()).count());
-    ImPlot::SetupAxisLimits(ImAxis_X1, currentTime - 10.0, currentTime, ImGuiCond_Always);
+    ImPlot::SetupAxisLimits(ImAxis_X1, currentTime - 300.0, currentTime, ImGuiCond_Always);
     ImPlot::SetupAxisScale(ImAxis_X1, ImPlotScale_Time);
     ImPlot::SetupAxis(ImAxis_Y2, "phi(deg)", ImPlotAxisFlags_AuxDefault);
     ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL, 0.5f);
@@ -98,21 +101,22 @@ void plotPower(std::vector<ScrollingBuffer> &signals) {
 void plotMainsFrequency(std::vector<ScrollingBuffer> &signals) {
     static ImPlotAxisFlags xflags = ImPlotAxisFlags_None;
     static ImPlotAxisFlags yflags = ImPlotAxisFlags_AutoFit | ImPlotAxisFlags_RangeFit;
-    ImPlot::SetupAxes("time (s)", "Frequency (Hz)", xflags, yflags);
+    ImPlot::SetupAxes("UTC Time", "Frequency (Hz)", xflags, yflags);
     auto   clock       = std::chrono::system_clock::now();
     double currentTime = static_cast<double>(std::chrono::duration_cast<std::chrono::seconds>(clock.time_since_epoch()).count());
-    ImPlot::SetupAxisLimits(ImAxis_X1, currentTime - 10.0, currentTime, ImGuiCond_Always);
+    ImPlot::SetupAxisLimits(ImAxis_X1, currentTime - 300.0, currentTime, ImGuiCond_Always);
     ImPlot::SetupAxisScale(ImAxis_X1, ImPlotScale_Time);
     ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL, 0.5f);
     // plotSignals(signals);
 }
 
-void plotPowerSpectrum(std::vector<Buffer> &signals, const bool violation, ImFont *fontawesome) {
+void plotPowerSpectrum(std::vector<Buffer> &signals, std::vector<Buffer> &limitingCurve, const bool violation, ImFont *fontawesome) {
     static ImPlotAxisFlags xflags = ImPlotAxisFlags_AutoFit | ImPlotAxisFlags_RangeFit;
     static ImPlotAxisFlags yflags = ImPlotAxisFlags_AutoFit | ImPlotAxisFlags_RangeFit;
     ImPlot::SetupAxes("Frequency (Hz)", "Power Density (dB)", xflags, yflags);
     ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL, 0.5f);
     plotSignals(signals);
+    plotSignals(limitingCurve);
     if (violation) {
         addPlotNotice("error: pulsed power limits exceeded!", fontawesome, ICON_FA_TRIANGLE_EXCLAMATION, { 1.0, 0, 0, 1.0 }, { 0.15, 0.05 });
         addPlotNotice("warning: pulsed power limits exceeded!", fontawesome, ICON_FA_TRIANGLE_EXCLAMATION, { 1, 0.5, 0, 1.0 }, { 0.15, 0.25 });
