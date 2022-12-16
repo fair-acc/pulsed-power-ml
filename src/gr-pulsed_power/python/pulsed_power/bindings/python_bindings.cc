2ff1ee1d2e86bab75ae5b4d36ace840f9d46e80c
carolatouchy <120090455+carolatouchy@users.noreply.github.com>
2022-12-16 11:15:52 +0100

    Gnu radio (#105)
    * Add GNU Radio flowgraphs
    
    * Add GR blocks (integration & statistics)
    
    * Add GNU Radio blocks (integration & statistics)
    
    * Add first test for integration block
    
    * Fix test setup in GNU Radio
    
    * Refactor statistics
    
    * update python bindings for statistics block
    
    * Add integration tests & bug fixes
    
    * add integration tests and bug fixing
    
    * update statistics block
    
    * update integration block
    
    * Restyled by clang-format
    
    * Add statistics tests
    
    * Remove unnecessary files
    
    * Restyled by clang-format
    
    * Fix binding of GR blocks
    
    * Add GR test execution to ci
    
    * change blocktypes
    
    * fixes merge
    
    Co-authored-by: C-N-E <christoph.neumann@infoteam.de>
    Co-authored-by: johannaheuser <johanna.heuser@infoteam.de>
    Co-authored-by: Sinisa Falamic <sinisa.falamic@infoteam.de>
    Co-authored-by: user1 <RonjaMichel>
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: johannaheuser <107842187+johannaheuser@users.noreply.github.com>
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Alexander Krimm <A.Krimm@gsi.de>

--- src/gr-pulsed_power/python/pulsed_power/bindings/python_bindings.cc
+++ src/gr-pulsed_power/python/pulsed_power/bindings/python_bindings.cc
@@ -22,6 +22,8 @@ namespace py = pybind11;
 /**************************************/
 // BINDING_FUNCTION_PROTOTYPES(
 void bind_opencmw_time_sink(py::module& m);
+void bind_integration(py::module& m);
+void bind_statistics(py::module& m);
 void bind_opencmw_freq_sink(py::module& m);
 // ) END BINDING_FUNCTION_PROTOTYPES
 
@@ -52,6 +54,7 @@ PYBIND11_MODULE(pulsed_power_python, m)
     /**************************************/
     // BINDING_FUNCTION_CALLS(
     bind_opencmw_time_sink(m);
+    bind_integration(m);
+    bind_statistics(m);
     bind_opencmw_freq_sink(m);
-    // ) END BINDING_FUNCTION_CALLS
 }
