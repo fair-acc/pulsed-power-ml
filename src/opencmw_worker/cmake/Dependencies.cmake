887e9499cbedbb224f0fa6070d06d3067379f579
krystynast <krystyna.stawikowska@infoteam.de>
2023-01-20 12:58:21 +0100

    Test for Integrator (#96)

--- src/opencmw_worker/cmake/Dependencies.cmake
+++ src/opencmw_worker/cmake/Dependencies.cmake
@@ -3,7 +3,8 @@ include(FetchContent)
 FetchContent_Declare(
         opencmw-cpp
         GIT_REPOSITORY https://github.com/fair-acc/opencmw-cpp.git
-        GIT_TAG main
+        # GIT_TAG main
+        GIT_TAG b5a1d125783ec8d8da62f7266bc7ebb63cc34ecb
 )
 
 # FetchContent_Declare(
--- src/opencmw_worker/src/NilmPredictWorker.hpp
+++ src/opencmw_worker/src/NilmPredictWorker.hpp
@@ -10,6 +10,7 @@
 #include <chrono>
 #include <unordered_map>
 
+#include "integrator/PowerIntegrator.hpp"
 #include <cppflow/cppflow.h>
 #include <cppflow/model.h>
 #include <cppflow/ops.h>
@@ -30,8 +31,8 @@ ENABLE_REFLECTION_FOR(NilmContext, ctx, contentType)
 struct NilmPredictData {
     int64_t                  timestamp;
     std::vector<double>      values; // = { 0.0, 25.7, 55.5, 74.1, 89.4, 34.5, 23.4, 1.0, 45.4, 56.5, 76.4, 23.8 };
-    std::vector<std::string> names  = { "device 1", "device 2", "device 3", "device 4", "device 5",
-         "device 6", "device 7", "device 8", "device 9", "device 10", "device 11", "others" };
+    std::vector<std::string> names = { "device 1", "device 2", "device 3", "device 4", "device 5",
+        "device 6", "device 7", "device 8", "device 9", "device 10", "device 11", "others" };
     std::vector<double>      day_usage;
     std::vector<double>      week_usage;
     std::vector<double>      month_usage;
@@ -69,18 +70,20 @@ class NilmPredictWorker
     : public Worker<serviceName, NilmContext, Empty, NilmPredictData, Meta...> {
 private:
     // const std::string  MODEL_PATH = "src/model/model_example";
-    const std::string               MODEL_PATH      = "src/model/TFGuptaModel_v1-0";
+    const std::string                MODEL_PATH      = "src/model/TFGuptaModel_v1-0";
 
-    std::shared_ptr<cppflow::model> _model          = std::make_shared<cppflow::model>(MODEL_PATH);
+    std::shared_ptr<cppflow::model>  _model          = std::make_shared<cppflow::model>(MODEL_PATH);
 
-    std::shared_ptr<PQSPhiDataSink> _pqsphiDataSink = std::make_shared<PQSPhiDataSink>();
-    std::shared_ptr<SUIDataSink>    _suiDataSink    = std::make_shared<SUIDataSink>();
+    std::shared_ptr<PQSPhiDataSink>  _pqsphiDataSink = std::make_shared<PQSPhiDataSink>();
+    std::shared_ptr<SUIDataSink>     _suiDataSink    = std::make_shared<SUIDataSink>();
 
-    bool                            init;
-    int64_t                         timestamp_frq;
+    bool                             init;
+    int64_t                          timestamp_frq;
 
-    std::mutex                      nilmTimeSinksRegistryMutex;
-    std::mutex                      nilmFrequencySinksRegistryMutex;
+    std::mutex                       nilmTimeSinksRegistryMutex;
+    std::mutex                       nilmFrequencySinksRegistryMutex;
+
+    std::shared_ptr<PowerIntegrator> _powerIntegrator;
 
 public:
     std::atomic<bool>     shutdownRequested;
@@ -96,7 +99,9 @@ public:
     explicit NilmPredictWorker(const BrokerType &broker,
             std::chrono::milliseconds            updateInterval)
         : super_t(broker, {}) {
-        notifyThread = std::jthread([this, updateInterval] {
+        _powerIntegrator = std::make_shared<PowerIntegrator>(nilmData.names);
+
+        notifyThread     = std::jthread([this, updateInterval] {
             std::chrono::duration<double, std::milli> pollingDuration;
             while (!shutdownRequested) {
                 std::chrono::time_point time_start = std::chrono::system_clock::now();
@@ -108,7 +113,7 @@ public:
 
                 try {
                     nilmData.timestamp = timestamp_n;
-                    //nilmData.values.clear();
+                    // nilmData.values.clear();
                     nilmData.day_usage.clear();
                     nilmData.week_usage.clear();
                     nilmData.month_usage.clear();
@@ -117,63 +122,55 @@ public:
                         std::scoped_lock lock_time_nilm(nilmTimeSinksRegistryMutex);
                         std::scoped_lock lock_freq_nilm(nilmFrequencySinksRegistryMutex);
 
-                        data_point = mergeValues();
+                        data_point                       = mergeValues();
 
-                        fmt::print("Size of data point {}\n", data_point.size());
+                        PQSPhiDataSink &pqsphiData       = *_pqsphiDataSink;
+
+                        int64_t         time_from_pqsphi = pqsphiData.timestamp;
+
+                        int64_t         size             = static_cast<int64_t>(data_point.size());
+                        cppflow::tensor input(data_point, { size });
+
+                        // fmt::print("tensor print: {}\n", tensor);
+
+                        fmt::print("input:  {}\n", input);
+
+                        auto output = (*_model)({ { "serving_default_args_0:0", input } }, { "StatefulPartitionedCall:0" });
+
+                        // int64_t size = static_cast<int64_t>(data_point.size());
+                        //   cppflow::tensor tensor(data_point, { size });
+                        // fmt::print("Tensor: {}\n", tensor);
+
+                        auto values = output[0].get_data<float>();
+
+                        // values print
+                        fmt::print("output: {}\n", output[0]);
+
+                        // fill data for REST
+                        nilmData.values.clear();
+
+                        // fill data for REST
+                        for (auto v : values) {
+                            nilmData.values.push_back(static_cast<double>(v));
+                        }
+
+                        // PQSPhiDataSink &pqsphiData       = *_pqsphiDataSink;
+
+                        // int64_t         time_from_pqsphi = pqsphiData.timestamp;
+
+                        _powerIntegrator->update(time_from_pqsphi, values);
                     }
-
-                    int64_t         size = static_cast<int64_t>(data_point.size());
-                    cppflow::tensor input(data_point, { size });
-
-                    //fmt::print("tensor print: {}\n", tensor);
-
-                    fmt::print("input:  {}\n", input);
-
-                    auto output = (*_model)({ { "serving_default_args_0:0", input } }, { "StatefulPartitionedCall:0" });
-
-                    //int64_t size = static_cast<int64_t>(data_point.size());
-                 //   cppflow::tensor tensor(data_point, { size });
-                    // fmt::print("Tensor: {}\n", tensor);
-
-                    auto values = output[0].get_data<float>();
-
-                    // values print
-                    fmt::print("output: {}\n", output[0]);
-
-                    // fill data for REST
-                    nilmData.values.clear();
-
-
-                     // fill data for REST
-                    for (auto v : values) {
-                        nilmData.values.push_back(static_cast<double>(v));
-                    } 
-
-                    //fmt::print("Output: {}\n", output);
-
                     fillDayUsage();
                     fillWeekUsage();
                     fillMonthUsage();
-                
-                }
-                 catch (const std::exception &ex) {
-                    fmt::print("caught exception '{}'\n", ex.what()); 
-                }
-             
-           /*      {//nilmData.values filled with dummy values                 
 
-                    // generierte dummy Werte
-                    for (std::size_t i=0;i<nilmData.names.size();i++){
-                        double value_new = nilmData.values.at(i) + 25.5;
-                        nilmData.values.at(i) = value_new<=100?value_new :  (value_new -100.0);
-                    }
-                    size_t zero_index = static_cast<size_t>( timestamp_n % 11);
-                    nilmData.values.at(zero_index) = 0.0;
-                } */
+                } catch (const std::exception &ex) {
+                    fmt::print("caught exception '{}'\n", ex.what());
+                }
 
                 super_t::notify("/nilmPredictData", context, nilmData);
 
-                pollingDuration   = std::chrono::system_clock::now() - time_start;
+                pollingDuration = std::chrono::system_clock::now() - time_start;
 
                 // TODO - check this - warning
                 auto willSleepFor = updateInterval - pollingDuration;
@@ -183,11 +180,11 @@ public:
                 //     fmt::print("Data prediction too slow\n");
                 // }
 
-                //std::this_thread::sleep_for(1000ms);
+                // std::this_thread::sleep_for(1000ms);
 
-                //std::this_thread::sleep_for(willSleepFor);
-		
-		if (willSleepFor > 0ms) {
+                // std::this_thread::sleep_for(willSleepFor);
+
+                if (willSleepFor > 0ms) {
                     std::this_thread::sleep_for(willSleepFor);
                 } else {
                     fmt::print("Data prediction too slow\n");
@@ -207,7 +204,7 @@ public:
 
             fmt::print("Name {}, unit {}, rate {}\n", signal_names, signal_units, sample_rate);
 
-            //std::vector<std::string> pqsohi_str{ "P", "Q", "S", "Phi" };
+            // std::vector<std::string> pqsohi_str{ "P", "Q", "S", "Phi" };
             if (signal_names == pqsohi_str) {
                 sink->set_callback(std::bind(&NilmPredictWorker::callbackCopySinkTimeData, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5));
             }
@@ -220,7 +217,7 @@ public:
             const auto signal_units = sink->get_signal_units();
             const auto sample_rate  = sink->get_sample_rate();
 
-            //std::vector<std::string> sui_str{ "S", "U", "I" };
+            // std::vector<std::string> sui_str{ "S", "U", "I" };
             if (signal_names == sui_str) {
                 fmt::print("Name {}, unit {}, rate {}\n", signal_names, signal_units, sample_rate);
                 sink->set_callback(std::bind(&NilmPredictWorker::callbackCopySinkFrequencyData, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4, std::placeholders::_5, std::placeholders::_6));
@@ -245,6 +242,8 @@ public:
 
     // copy P Q S Phi data
     void callbackCopySinkTimeData(std::vector<const void *> &input_items, int &noutput_items, const std::vector<std::string> &signal_names, float /* sample_rate */, int64_t timestamp_ns) {
+        fmt::print("--callbackCopySinkTimeData: timestamp: {}\n", timestamp_ns);
+
         const float             *p   = static_cast<const float *>(input_items[0]);
         const float             *q   = static_cast<const float *>(input_items[1]);
         const float             *s   = static_cast<const float *>(input_items[2]);
@@ -260,7 +259,8 @@ public:
             _pqsphiDataSink->s         = *(s + noutput_items - 1);
             _pqsphiDataSink->phi       = *(phi + noutput_items - 1);
 
-            fmt::print("PQSPHI {} {} {} {}\n", _pqsphiDataSink->p, _pqsphiDataSink->q, _pqsphiDataSink->s, _pqsphiDataSink->phi);
+            fmt::print("PQSPHI {} {} {} {}, timestamp {}\n", _pqsphiDataSink->p, _pqsphiDataSink->q, _pqsphiDataSink->s, _pqsphiDataSink->phi,
+                    _pqsphiDataSink->timestamp);
         }
     }
 
@@ -364,23 +364,22 @@ private:
         return output;
     }
 
-    void fillDayUsage(){
+    void fillDayUsage() {
         nilmData.day_usage.clear();
         // dummy data
-        nilmData.day_usage = { 100.0,323.34,234.33, 500.55, 100.0,323.34,234.33, 500.55, 100.0,323.34,234.33, 21.9};
+        nilmData.day_usage = { 100.0, 323.34, 234.33, 500.55, 100.0, 323.34, 234.33, 500.55, 100.0, 323.34, 234.33, 21.9 };
     }
 
-    void fillWeekUsage(){
+    void fillWeekUsage() {
         nilmData.week_usage.clear();
-        //dummy data
-        nilmData.week_usage = { 700.0, 2123.34, 1434.33, 3500.55, 700.0, 2123.34, 1434.33, 3500.55,700.0,2123.34,1434.33,100.0};
+        // dummy data
+        nilmData.week_usage = { 700.0, 2123.34, 1434.33, 3500.55, 700.0, 2123.34, 1434.33, 3500.55, 700.0, 2123.34, 1434.33, 100.0 };
     }
 
-    void fillMonthUsage(){
+    void fillMonthUsage() {
         nilmData.month_usage.clear();
-        //dummy data
-        nilmData.month_usage =  {1500.232, 3000.99, 2599.34, 1200.89, 1500.232, 3000.99, 2599.34, 1200.89,1500.232, 3000.99, 2599.34 ,300.0};
-
+        // dummy data
+        nilmData.month_usage = { 1500.232, 3000.99, 2599.34, 1200.89, 1500.232, 3000.99, 2599.34, 1200.89, 1500.232, 3000.99, 2599.34, 300.0 };
     }
 
     // Test - dummy vector
