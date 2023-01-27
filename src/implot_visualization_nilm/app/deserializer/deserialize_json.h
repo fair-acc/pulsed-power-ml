527eff4792404e9c0fcceed34e4215a7da41ad56
krystynast <krystyna.stawikowska@infoteam.de>
2023-01-27 15:02:59 +0100

    Nilm dashboard - initialisation of usage values - bugfix

--- src/implot_visualization_nilm/app/deserializer/deserialize_json.h
+++ src/implot_visualization_nilm/app/deserializer/deserialize_json.h
@@ -5,7 +5,6 @@
 #include <shared_mutex>
 #include <vector>
 
-
 class DataPoint {
 public:
     double x;
@@ -17,7 +16,7 @@ public:
 
 class Buffer {
 public:
-    // TODO 
+    // TODO
     ImVector<DataPoint> data;
     std::string         signalName;
 
@@ -33,7 +32,7 @@ public:
     ImVector<DataPoint> data;
     std::string         signalName;
 
-    ScrollingBuffer(int max_size = 500'000); //200'000
+    ScrollingBuffer(int max_size = 500'000); // 200'000
 
     void addPoint(double x, double y);
     void erase();
@@ -41,18 +40,17 @@ public:
 
 class BufferPower {
 public:
-    bool                   init = false;
-    std::vector<double>    values;
-    double                 timestamp;   
+    bool                init = false;
+    std::vector<double> values;
+    double              timestamp;
 
-    void updateValues(const std::vector<double> &_values);
+    void                updateValues(const std::vector<double> &_values);
 };
 
 struct StrideArray {
     std::vector<int>    dims;
     std::vector<double> values;
 };
-
 
 class Acquisition {
 public:
@@ -61,24 +59,24 @@ public:
     uint64_t                     lastRefTrigger = 0;
     std::vector<ScrollingBuffer> buffers;
     bool                         success = false;
-    bool                         init = false;
-       
+    bool                         init    = false;
+
     Acquisition();
     Acquisition(int numSignals);
 
-    void deserialize(); 
-    void fail(){this->success = false;};
+    void                deserialize();
+    void                fail() { this->success = false; };
 
     std::vector<double> relativeTimestamps;
     uint64_t            lastTimeStamp = 0.0;
 
 private:
-    uint64_t            refTrigger_ns = 0;
-    double              refTrigger_s  = 0.0;
- 
-    StrideArray         strideArray;
+    uint64_t    refTrigger_ns = 0;
+    double      refTrigger_s  = 0.0;
 
-    void                addToBuffers(); 
+    StrideArray strideArray;
+
+    void        addToBuffers();
 };
 
 class AcquisitionSpectra {
@@ -91,8 +89,8 @@ public:
     AcquisitionSpectra();
     AcquisitionSpectra(int numSignals);
 
-    void deserialize();
-    void fail(){};
+    void                deserialize();
+    void                fail(){};
 
     std::vector<double> relativeTimestamps = { 0 };
     uint64_t            lastTimeStamp      = 0.0;
@@ -108,57 +106,55 @@ private:
 
 class PowerUsage {
 public:
-    //dummy devices
-    std::vector<std::string> devices; // = {"Device 1", "Device 2", "Sys Laptop", "Lamp", "Light", "Fridge","Fan","TV","PC","Radio",  "Others"}; 
-    std::vector<double> powerUsages;
-    std::vector<double> powerUsagesDay;
-    std::vector<double> powerUsagesWeek;
-    std::vector<double> powerUsagesMonth;
-    uint64_t            lastRefTrigger = 0;
-    std::string         jsonString = "";
-    std::vector<Buffer> buffers;
-    bool                success = false;
-    bool                init = false; 
-    double              deliveryTime;
-    int64_t             timestamp;
-    uint64_t            lastTimeStamp      = 0.0;
-
+    // dummy devices
+    std::vector<std::string> devices; // = {"Device 1", "Device 2", "Sys Laptop", "Lamp", "Light", "Fridge","Fan","TV","PC","Radio",  "Others"};
+    std::vector<double>      powerUsages;
+    std::vector<double>      powerUsagesDay;
+    std::vector<double>      powerUsagesWeek;
+    std::vector<double>      powerUsagesMonth;
+    uint64_t                 lastRefTrigger = 0;
+    std::string              jsonString     = "";
+    std::vector<Buffer>      buffers;
+    bool                     success = false;
+    bool                     init    = false;
+    double                   deliveryTime;
+    int64_t                  timestamp;
+    uint64_t                 lastTimeStamp = 0.0;
 
     // dummy values
-    std::vector<double> lastWeekUsage   = {85.0, 64.0, 56.0, 97.0, 79.0, 71.0, 20.0}; // other service
-
-     // dummy value
-    double              powerUsageToday ; //= 45.9;
-
-    //dummy value
-    double              kWhUsedDay   ;//   = 1098.99;
-
-    //dummy value
-    double              kWhUsedMonth   ;// = 1098.99;
-
-    //dummy value
-    double              costPerMonth  ;//  = 3919.45;
+    std::vector<double> lastWeekUsage = { 85.0, 64.0, 56.0, 97.0, 79.0, 71.0, 20.0 }; // other service
 
     // dummy value
-    double              kWhUsedWeek;//    = 398.99;
+    double powerUsageToday = 0.0; //= 45.9;
 
-    // dummy value 
-    double              costPerWeek; //     = 1238.98;
- 
+    // dummy value
+    double kWhUsedDay = 0.0; //   = 1098.99;
+
+    // dummy value
+    double kWhUsedMonth = 0.0; // = 1098.99;
+
+    // dummy value
+    double costPerMonth = 0.0; //  = 3919.45;
+
+    // dummy value
+    double kWhUsedWeek = 0.0; //    = 398.99;
+
+    // dummy value
+    double costPerWeek = 0.0; //     = 1238.98;
 
     PowerUsage();
     PowerUsage(int numSignals);
 
-    void                deserialize();
-    void                fail();
-    double              sumOfUsage();
+    void   deserialize();
+    void   fail();
+    double sumOfUsage();
 
-private: 
-    void                setSumOfUsageDay();
-    void                setSumOfUsageWeek();
-    void                setSumOfUsageMonth();
+private:
+    void setSumOfUsageDay();
+    void setSumOfUsageWeek();
+    void setSumOfUsageMonth();
 
-// // TODO - define Buffer if needed
-// private:
-//     void                addToBuffers();  
+    // // TODO - define Buffer if needed
+    // private:
+    //     void                addToBuffers();
 };
