6d5fbc739456dc9b8d6733a04f2a540379686c2d
Ronja Michel <RonjaMichel>
2023-02-14 09:11:01 +0100

    Add tests

--- src/implot_visualization/test/deserialize_json_tests.cpp
+++ src/implot_visualization/test/deserialize_json_tests.cpp
@@ -3,14 +3,37 @@
 // #include <imgui.h>
 #include <vector>
 
-TEST_CASE("Test Case", "[ScrollingBuffer]") {
-    /*ScrollingBuffer scrollingBuffer(2);
+TEST_CASE("Default Scrolling Buffer", "[ScrollingBuffer]") {
+    ScrollingBuffer scrollingBuffer;
+    REQUIRE(scrollingBuffer.maxSize == 200000);
+}
+/*TEST_CASE("Scrolling Buffer with more points", "[ScrollingBuffer]") {
+    ScrollingBuffer scrollingBuffer(2);
     scrollingBuffer.addPoint(1, 2);
     scrollingBuffer.addPoint(3, 4);
     scrollingBuffer.addPoint(5, 6);
-    // ImVector<DataPoint> testData{ DataPoint(3, 4), DataPoint(5, 6) };
+
+    ImVector<DataPoint> testData;
+    DataPoint           d1(3, 4); // wie deklariert man Datenpunkte
+    DataPoint           d2(5, 6);
+    testData.push_back(d1);
+    testData.push_back(d2);
 
     REQUIRE(scrollingBuffer.maxSize == 2);
-    // REQUIRE(scrollingBuffer.data == testData); */
-    REQUIRE(0 == 0);
+    REQUIRE(scrollingBuffer.data == testData); // wie vergleicht man den Inhalt
+}*/
+TEST_CASE("Empty Scrolling Buffer", "[ScrollingBuffer]") {
+    ScrollingBuffer scrollingBuffer(0);
+    scrollingBuffer.addPoint(1, 2);
+    scrollingBuffer.addPoint(3, 4);
+
+    REQUIRE(scrollingBuffer.maxSize == 0);
+}
+TEST_CASE("Deleted Scrolling Buffer", "[ScrollingBuffer]") {
+    ScrollingBuffer scrollingBuffer(2);
+    scrollingBuffer.addPoint(1, 2);
+    scrollingBuffer.addPoint(3, 4);
+    scrollingBuffer.erase();
+
+    REQUIRE(scrollingBuffer.maxSize == 0);
 }
