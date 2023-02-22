c1277192f7a09e219ffa41fd6f793cbf8f2a8e99
johannaheuser <johanna.heuser@infoteam.de>
2023-02-22 09:20:05 +0100

    GNU Radio: remove senseless test

--- src/gr-pulsed_power/lib/qa_power_calc_cc.cc
+++ src/gr-pulsed_power/lib/qa_power_calc_cc.cc
@@ -22,21 +22,6 @@ BOOST_AUTO_TEST_CASE(test_power_calc_cc_Calc_active_power_Basic_input)
     calc_block->calc_active_power(out, voltage, current, phi_out, noutput_items);
     BOOST_CHECK(out[0] == (float)(1 * 1 * cos(0)));
 }
-BOOST_AUTO_TEST_CASE(test_power_calc_cc_Calc_active_power_NAN_input)
-{
-    auto calc_block = gr::pulsed_power::power_calc_cc::make(0.0001);
-    float out_float[1] = { 0 };
-    float* out = out_float;
-    float float_voltage[1] = { nanf("") };
-    float* voltage = float_voltage;
-    float float_current[1] = { 1 };
-    float* current = float_current;
-    float float_phi_out[1] = { 0 };
-    float* phi_out = float_phi_out;
-    int noutput_items = 1;
-    calc_block->calc_active_power(out, voltage, current, phi_out, noutput_items);
-    BOOST_CHECK(out[0] == (float)(1 * 1 * cos(0)));
-}
 BOOST_AUTO_TEST_SUITE_END();
 } // namespace pulsed_power
 } /* namespace gr */
